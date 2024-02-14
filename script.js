1711c24a41f3127103209bd50cc4aad0a153de82
Mona <nairamonteiro3@gmail.com>
2024-02-14 10:04:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let popup = document.getElementById('popup');
+
+function abrePopup() {
+    popup.classList.add('open-popup');
+}
+
+function fechaPopup() {
+    popup.classList.remove('open-popup');
+}
